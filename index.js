a8f52d03c6a7159c22f1108f649c9e09eae242e6
hemashreesahadev-rgb <hemashreesahadev@gmail.com>
2026-01-11 13:02:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,4 +36,8 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server is flying on port ${PORT}`);
+
+});
+app.get('/', (req, res) => {
+    res.send("Smart Campus API is officially LIVE!");
 });
